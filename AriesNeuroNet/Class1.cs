aab5cb09e3b29b8aff8f38d3cb88371a2e894276
George Bora <gpbora@gmail.com>
2014-03-02 14:05:05 +0200

    second commit

---
+++ AriesNeuroNet/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AriesNeuroNet
+{
+    public class Class1
+    {
+        Random rand = new Random();
+    }
+}
